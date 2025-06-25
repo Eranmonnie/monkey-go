8214dbd3bce70ae42b15afe905f93415d529e483
Eranmonnie <ajalaferanmi123@gmail.com>
2025-06-25 16:17:44 +0100

    return statement evaluation

--- object/object.go
+++ object/object.go
@@ -5,10 +5,18 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETUEN_VALUE"
 )
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 
 type Object interface {
 	Type() ObjectType
