2904fd7e929cef6cf083e65107d1c14f3c784be9
Eranmonnie <ajalaferanmi123@gmail.com>
2025-08-11 20:14:53 +0100

    added string concatination support and makefile

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -188,6 +188,10 @@ func TestErrorHandling(t *testing.T) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
+		{
 			`
 	if (10 > 1) {
 	if (10 > 1) {
@@ -261,6 +265,17 @@ func TestStringLiteral(t *testing.T) {
 	}
 }
 
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
 func TestFunctionApplication(t *testing.T) {
 	tests := []struct {
 		input    string
